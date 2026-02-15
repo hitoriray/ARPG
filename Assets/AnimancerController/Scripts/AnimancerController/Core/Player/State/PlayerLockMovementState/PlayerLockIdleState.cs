7f768a5996366ddcadf407c15d3cf3ca4f54be6f
ray <676155220@qq.com>
2026-02-15 16:27:35 +0800

    删掉Animancer,使用Invector第三人称控制器

--- Assets/AnimancerController/Scripts/AnimancerController/Core/Player/State/PlayerLockMovementState/PlayerLockIdleState.cs
+++
@@ -1,6 +0,0 @@
-﻿public class PlayerLockIdleState : PlayerMovementState
-{
-    public PlayerLockIdleState(PlayerStateMachine stateMachine) : base(stateMachine)
-    {
-    }
-}
--- Assets/AnimancerController/Scripts/AnimancerController/Core/StateMachine/IState/IState.cs
+++
@@ -1,8 +0,0 @@
-public interface IState
-{
-    void OnEnter();
-    void OnUpdate();
-    void OnAnimationUpdate();
-    void OnExit();
-    void OnAnimationEnd();
-}
